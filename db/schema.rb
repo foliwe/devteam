661bd82c2532f34b5a05a604cdcd274e3f8a06d3
foliwe <foliwe@gmail.com>
2018-05-25 13:25:10 +0100

    sign up plan views

--- db/schema.rb
+++ db/schema.rb
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 20180525114539) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "plan_id", default: 0
+    t.integer "plan_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
