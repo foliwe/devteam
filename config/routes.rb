c34671b577476fb716b38a495465d98f234b4502
foliwe <foliwe@gmail.com>
2018-05-25 01:35:35 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   get 'about', to: 'pages#about'
-  get 'contact-us', to: 'contacts#new', as: 'contact-us'
+  get 'contact_us', to: 'contacts#new', as: 'contact_us'
 end
