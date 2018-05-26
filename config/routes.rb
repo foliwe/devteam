6df56cd95b3d9a31c270a7a65d1ae365fd3212b9
foliwe <foliwe@gmail.com>
2018-05-26 15:34:13 +0100

    subscription working

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   resources :contacts ,only: :create
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+  devise_for :users,controllers:{registrations: 'users/registrations'}
   get 'about', to: 'pages#about'
   get 'contact_us', to: 'contacts#new', as: 'contact_us'
 end
