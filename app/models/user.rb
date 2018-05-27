0cce5966b7329e5b7120d93a4bf67ac66c0bb0f0
foliwe <foliwe@gmail.com>
2018-05-27 11:09:50 +0100

    add user profile relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 # association
   belongs_to :plan
-attr_accessor :stripe_card_token
+  has_one :profile
+  attr_accessor :stripe_card_token
 
 
   def save_with_subscription
